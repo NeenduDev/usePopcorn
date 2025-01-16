e4762f9fdbb3787aaa8a59c7aed1eade9ffb2acf
NeenduDev <neendu.wickremasinghe@gmail.com>
2025-01-16 15:10:25 +0530

    feat: enhance StarRating component with flexible props and improved reusability
    - Added `PropTypes` validation for the `StarRating` component to define and validate supported props.
    - Introduced new props:
      - `color`: Allows customization of star color.
      - `size`: Adjusts the size of the stars.
      - `messages`: Displays custom messages corresponding to each rating.
      - `defaultRating`: Sets an initial rating value.
      - `onSetRating`: Callback function triggered on rating selection.
      - `className`: Enables custom CSS class application.
    - Modified star rendering to dynamically adjust color and size based on props.
    - Updated text display logic to show relevant messages or ratings.
    - Refactored `Star` component to use prop-driven styles.
    - Added a `Test` component in `index.js` to demonstrate usage of the `StarRating` component with different props.
    - Improved example renderings in `index.js` to showcase new functionality.
    
    This update makes the `StarRating` component more versatile and adaptable for various use cases.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} maxRating="10" />
+      <p>This movie was rated {movieRating} starts</p>
+    </div>
+  );
+}
+// console.log("NODE_ENV:", process.env.NODE_ENV);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      testProp={3}
+      maxRating={10}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={0} />
+    <Test />
   </React.StrictMode>
 );
